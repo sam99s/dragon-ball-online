e78b04eab7a79162279a3a774319e4814220b21a
sam99s <samucarp99@gmail.com>
2021-08-03 01:32:39 -0300

    agregando personajes dbz saga saiyajin :memo:

--- js/personajes.js
+++ js/personajes.js
@@ -1,14 +1,32 @@
-$(document).ready(function () {    
+$(document).ready(function () {   
+
+    /* Personaje de Dragon Ball */
     const figura = $('.db-imagen-figura')
     let foto = $(".db-imagen-figura_personaje")
 
     $('.character-db').click((e) => {
         $(foto).remove();
         let seleccionado = e.target.id;
+        let seleccionadoValue = e.target.value;
         $(figura).append(`
-        <img class="db-imagen-figura_personaje animate__animated animate__fadeIn" src="../assets/personajes/db/${seleccionado}.png" alt="goku"></img>
+        <img class="db-imagen-figura_personaje animate__animated animate__fadeIn" src="../assets/personajes/db/${seleccionado}.png" alt="${seleccionadoValue}"></img>
         `)
         foto = $(".db-imagen-figura_personaje")
+    })
+
+    /* Personaje de Dragon Ball - Saga Saiyajin */
+
+    const figurass = $('.dbzss-imagen-figurass');
+    let fotosss = $('.dbzss-imagen-figurass_personajess')
+
+    $('.character-dbzss').click((e) => {
+        $(fotosss).remove();
+        let seleccionadoss = e.target.id;
+        let seleccionadossValue = e.target.value;
+        $(figurass).append(`
+        <img class="dbzss-imagen-figurass_personajess animate__animated animate__fadeIn" src="../assets/personajes/dbz/ss/${seleccionadoss}.png" alt="${seleccionadossValue}"></img>
+        `)
+        fotosss = $(".dbzss-imagen-figurass_personajess")
     })
 
 })
